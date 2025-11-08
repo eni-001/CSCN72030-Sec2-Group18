6d4c441eaa5a96239b56956317a58ff5ad359243
LayiDawo <olayiwoladawodu@icloud.com>
2025-11-08 00:20:26 -0500

    Initialized a CameraController.cs Boilerplate

---
+++ CameraController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHome.Domain
+{
+    public class CameraController : IDevice
+    {
+        public string Name { get; }
+        public bool IsStreaming { get; private set; }
+        public DateTime? LastSnapshotUtc { get; private set; }
+        public string Status => IsStreaming
+            ? $"Streaming (snapshot {LastSnapshotUtc:HH:mm:ss} UTC)"
+            : "Idle";
+
+        public CameraController(string name = "Front Door Camera")
+        {
+            Name = name;
+        }
+
+        public void Start() => IsStreaming = true;
+        public void Stop() => IsStreaming = false;
+        public void Snapshot() => LastSnapshotUtc = DateTime.UtcNow;
+    }
+}
