7b48eaef62ff61c20fda4ab17ae216cb0abfee55
LannaTikum <tikumlanna@gmail.com>
2025-11-08 00:14:37 -0500

    Initialized a SmokeDetector.cs Boilerplate

---
+++ SmokeDetector.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHome.Domain
+{
+    public class SmokeDetector : IDevice
+    {
+        public string Name { get; }
+        public bool SmokeDetected { get; private set; }
+        public string Status => SmokeDetected ? "SMOKE ALERT" : "Clear";
+
+        public SmokeDetector(string name = "Kitchen Smoke")
+        {
+            Name = name;
+        }
+
+        public void Trigger() => SmokeDetected = true;
+        public void Clear() => SmokeDetected = false;
+    }
+}
