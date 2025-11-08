a0a474f4b0d80fd374b338d27bceec727232d5be
Aya Ben Medjeber <Abenmedjeber3714@conestogac.on.ca>
2025-11-08 00:17:17 -0500

    Initialized a DoorLockController.cs Boilerplate

---
+++ DoorLockController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHome.Domain
+{
+    public class DoorLockController : IDevice
+    {
+        public string Name { get; }
+        public bool IsLocked { get; private set; }
+        public string Status => IsLocked ? "Locked" : "Unlocked";
+
+        public DoorLockController(string name = "Main Door")
+        {
+            Name = name;
+            IsLocked = true;
+        }
+
+        public void Lock() => IsLocked = true;
+        public void Unlock() => IsLocked = false;
+    }
+}
