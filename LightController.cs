e2927d7766f317d769daf17e12808a83523788f4
eni-001 <Eolasunkanmi9458@conestogac.on.ca>
2025-11-08 00:11:30 -0500

    Initialized LightController.cs boilerplate

---
+++ LightController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHome.Domain
+{
+    public class LightController : IDevice
+    {
+        public string Name { get; }
+        public bool IsOn { get; private set; }
+        public string Status => IsOn ? "ON" : "OFF";
+
+        public LightController(string name = "Living Room Light")
+        {
+            Name = name;
+        }
+
+        public void TurnOn() => IsOn = true;
+        public void TurnOff() => IsOn = false;
+        public void Toggle() => IsOn = !IsOn;
+    }
+}
+
