e4da020eef61adf0df5bac361f25daff931fad04
LayiDawo <olayiwoladawodu@icloud.com>
2025-11-08 00:19:51 -0500

    Initialized a HeaterController.cs Boilerplate

---
+++ HeaterController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHome.Domain
+{
+    public class HeaterController : IDevice
+    {
+        public string Name { get; }
+        public double SetpointC { get; private set; }
+        public bool IsHeating { get; private set; }
+        public string Status => IsHeating ? $"Heating to {SetpointC:0.0}°C" : $"Idle (set {SetpointC:0.0}°C)";
+
+        private readonly TemperatureSensor _sensor;
+
+        public HeaterController(TemperatureSensor sensor, string name = "Heater")
+        {
+            Name = name;
+            _sensor = sensor;
+            SetpointC = 22.0;
+        }
+
+        public void SetSetpoint(double celsius) => SetpointC = celsius;
+
+        public void Tick()
+        {
+            IsHeating = _sensor.CurrentCelsius < SetpointC - 0.2;
+            if (IsHeating) _sensor.Nudge(0.3);
+        }
+    }
+}
+
